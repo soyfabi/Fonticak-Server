93588a60be15e9a4770af477eb41fa16cdddf1a6
soyfabi <fabi.marzan1762@yahoo.com>
2023-05-05 01:14:53 -0400

    Upgrade Moveitem

--- data/scripts/eventcallbacks/player/default_onMoveItem.lua
+++ data/scripts/eventcallbacks/player/default_onMoveItem.lua
@@ -15,6 +15,15 @@ event.onMoveItem = function(self, item, count, fromPosition, toPosition, fromCyl
 		return false
 	end
 	
+	-- Player Corpse Container --
+	if fromCylinder and fromCylinder:isItem() then
+        if fromCylinder:getId() == 3065 or 3058 then
+            if toCylinder ~= fromCylinder then
+                self:say(string.format("x%d %s", count, item:getName()), TALKTYPE_MONSTER_SAY, false, nil, fromCylinder:getPosition())
+            end
+        end
+    end
+	
 	-- No move parcel very heavy
 	if CONTAINER_WEIGHT_CHECK and ItemType(item:getId()):isContainer()
 	and item:getWeight() > CONTAINER_WEIGHT_MAX then
